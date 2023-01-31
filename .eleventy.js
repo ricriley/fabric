a397c197fca11dc2d2910a2465217aa166426840
ricriley <ricriley@gmail.com>
2023-01-31 20:26:48 +0000

    Fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("static");
 
   return {
     passthroughFileCopy: true,
